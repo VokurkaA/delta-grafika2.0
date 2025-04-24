6a451748c85613779f488f6017de81dfefbf4d6a
Vokurkaa <zadrobilek.jan@seznam.cz>
2025-04-24 09:24:19 +0200

    changes

--- src/models/drawable/shape/Shape.java
+++ src/models/drawable/shape/Shape.java
@@ -17,6 +17,13 @@ public abstract class Shape extends Drawable {
 
     public abstract void move(Point point, DrawingParams drawingParams, boolean newPoint);
 
+    public void moveEntireShape(Point origin, Point destination) {
+        Point moveVector = new Point(destination.getX() - origin.getX(), destination.getY() - origin.getY());
+        for (Point point : points) {
+            point.set(Point.add(point, moveVector));
+        }
+    }
+
     public abstract void rasterize(Graphics g);
 
     public abstract double getNearestDistance(Point click);
